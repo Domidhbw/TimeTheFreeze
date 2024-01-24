dad7e2e045ac043891ff6c9cd76f5951c877dd3d
Domidhbw <dom.neubauer.23@lehre.mosbach.dhbw.de>
2024-01-24 19:17:31 +0100

    fixed errors and moved files

--- Main.py
+++ Main.py
@@ -1,23 +1,33 @@
 import pygame
-from listGenerator import LevelGenerator
+from LevelEditor.listGenerator import LevelGenerator
 
 pygame.init()
-screen = pygame.display.set_mode((1200, 600))
+screen = pygame.display.set_mode((1800, 900))
 clock = pygame.time.Clock()
 running = True
 
-level = LevelGenerator("testLevel.txt",pygame.display.get_surface())
+
+#initialize class
+level = LevelGenerator("LevelEditor/testLevel.txt",pygame.display.get_surface())
+
+
 
 while running:
+    #GET INPUT
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     
+
+
+    #GAMELOOP
+
+
+    # RENDER YOUR GAME HERE
     for tile in level.level:
         tile.rectangle.update(tile.rectangle.left,tile.rectangle.top,level.tileScale[1],level.tileScale[0])
         tile.draw(screen)
-    # RENDER YOUR GAME HERE
+
 
     pygame.display.flip()
 
