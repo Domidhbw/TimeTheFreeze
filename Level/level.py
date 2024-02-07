3e977d2cc833bd4dab865de72a75837ff7d64d92
Domidhbw <dom.neubauer.23@lehre.mosbach.dhbw.de>
2024-02-07 11:50:14 +0100

    Revert "update level.py"
    This reverts commit 2443b4160c210a7a8e88b3060397e4b5a5e180e7.

--- Level/level.py
+++ Level/level.py
@@ -1,4 +1,5 @@
 level_map=[
+<<<<<<< Updated upstream
 '                     f  f                             ',
 '                      ff                              ',
 '           s          ff                              ',
@@ -13,6 +14,22 @@ level_map=[
 'xxx              xxxxx             iii     x x        ',
 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx        ',
 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx        ',
+=======
+'                           ',
+'                           ',
+'                           ',
+'    wvvvx                  ',
+'    immmy                  ',
+'                           ',
+'                           ',
+'           wj              ',
+'           zy          wvx ',
+'                     wvtun ',
+'                   wvtuuun ',
+' wx         f wvkvvtuuuuun ',
+'vtsvx  wvkkkvvtuuuuuuuuuun ',
+'uuuun  puuuuuuuuuuuuuuuuun ',
+>>>>>>> Stashed changes
 ]
 tiles = {
     'o': './assets/black.png',
